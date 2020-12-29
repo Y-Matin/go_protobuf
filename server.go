7addf2482e894cdb1011523d067d82609a00cc07
yedasheng@ebupt.com <724797522@qq.com>
2020-12-29 16:30:37 +0800

    1

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	rpcServer := grpc.NewServer()
 	service.RegisterProdServiceServer(rpcServer, new(service.ProdService))
-	listen, _ := net.Listen("tcp", ":8080")
+	listen, _ := net.Listen("tcp", ":8081")
 	rpcServer.Serve(listen)
 
 }
--- service/ProdService.go
+++ service/ProdService.go
@@ -6,5 +6,5 @@ type ProdService struct {
 }
 
 func (p *ProdService) GetProdStock(context.Context, *ProdRequest) (*ProdResponse, error) {
-	return new(ProdResponse), nil
+	return &ProdResponse{ProdStock: 20}, nil
 }
