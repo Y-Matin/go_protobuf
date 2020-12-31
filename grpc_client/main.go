6895749cbe7a32fe0bb628154cb4bf93dff0735a
yedasheng@ebupt.com <724797522@qq.com>
2020-12-31 16:03:20 +0800

    add 服务端证书

--- grpc_client/main.go
+++ grpc_client/main.go
@@ -4,23 +4,25 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"main/service"
 )
 
 func main() {
-
-
-
-	conn, err := grpc.Dial(":8081", grpc.WithInsecure())
+	creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "yeds")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
 	client := service.NewProdServiceClient(conn)
-	prodResponse, err := client.GetProdStock(context.Background(), &service.ProdRequest{ProdId: 20})
-	if err!= nil {
+	prodResponse, err := client.GetProdStock(context.Background(), &service.ProdRequest{ProdId: 10})
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Print(prodResponse.ProdStock)
