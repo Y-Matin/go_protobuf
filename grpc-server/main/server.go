7b734fe86e18538b4f2d4b1757fa2a3c50ad6518
Y-Martin <724797522@qq.com>
2021-01-03 15:56:58 +0800

    add  tls certifacation success

--- grpc-server/main/server.go
+++ grpc-server/main/server.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"log"
+	"io/ioutil"
 	"main/service"
 	"net"
 )
 
 func main() {
-	creds, err := credentials.NewServerTLSFromFile("key/server.crt", "keys/server_no_passwd.key")
-	if err != nil {
-		log.Fatal(err)
-	}
+	/*	creds, err := credentials.NewServerTLSFromFile("key/server.crt", "keys/server_no_passwd.key")
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	cert, _ := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
+	certPool := x509.NewCertPool()
+	ca, _ := ioutil.ReadFile("cert/ca.pem")
+	certPool.AppendCertsFromPEM(ca)
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	})
 
 	rpcServer := grpc.NewServer(grpc.Creds(creds))
 	service.RegisterProdServiceServer(rpcServer, new(service.ProdService))
